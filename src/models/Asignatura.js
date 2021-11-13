d2d0910757bee91b731db52e47270fcd40c267cd
Carlos <alu0101102726@ull.edu.es>
2021-11-13 11:17:36 +0000

    Modificando modelo Asignatura mongoose a mysql

--- src/models/Asignatura.js
+++ src/models/Asignatura.js
@@ -1,16 +1,26 @@
-const mongoose = require('mongoose');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
-const Asignatura = new mongoose.Schema({
-    cod_asig: Number,
-    cod_titulo: Number,
-    profesor: Array,
-    dificultad: String, // yo esto no lo pondria | esto daba error AnalyserNode
-    cred_asig: Number,
-    nom_asig: String,
-    curso: Number,
-    cuatrimestre: Number,
-    tip_asig: String,
-    especial: Boolean,
-});
+const Asignatura = (sequelize, Sequelize) => {
+    const {INTEGER, STRING, ARRAY, BOOLEAN} = Secuelize;
+    const Asignatura = sequelize.define('Asignatura', {
+        cod_asig: {type: INTEGER, primaryKey: true},
+        cod_titulo: {
+            type: INTEGER,
+            references: {
+                model: Titulacion,
+                key: 'cod_titulo'
+            }
+        },
+        profesor: {type: ARRAY, primaryKey: false},
+        dificultad: {type: STRING, primaryKey: false},
+        cred_asig: {type: NUMBER, primaryKey: false},
+        nom_asig: {type: STRING, primaryKey: false},
+        curso: {type: NUMBER, primaryKey: false},
+        cuatrimestre: {type: NUMBER, primaryKey: false},
+        tip_asig: {type: STRING, primaryKey: false},
+        especial: {type: BOOLEAN, primaryKey: false},
+    });
+}
 
-module.exports = mongoose.model('Asignatura', Asignatura);
+module.exports = Asignatura;
