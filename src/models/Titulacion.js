eafcdab6d465a89dfc62ba81ae48a5ae9c5463b0
Carlos <alu0101102726@ull.edu.es>
2021-11-13 11:45:05 +0000

    Modificando modelo mongoose a mysql

--- src/models/Titulacion.js
+++ src/models/Titulacion.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
-const Titulacion = new mongoose.Schema({
-    cod_titul: Number,
-    asignaturas: Array,
-    num_cursos: Number,
-    tip_titul: String,
-    tip_estud: String ,
-    total_cred: Number,
-    p_abandono: Number
-});
+const Titulacion = (sequelize, Sequelize) => {
+    const {INTEGER, STRING, ARRAY} = Secuelize;
+    const Titulacion = sequelize.define('Titulacion', {
+        cod_titul: {type: INTEGER, primaryKey: true},
+        asignaturas: {type: ARRAY, primaryKey: false},
+        num_cursos: {type: NUMBER, primaryKey: false},
+        tip_titul: {type: STRING, primaryKey: false},
+        tip_estud: {type: STRING, primaryKey: false},
+        total_cred: {type: NUMBER, primaryKey: false},
+        p_abandono: {type: NUMBER, primaryKey: false}
+    });
+}
 
-module.exports = mongoose.model('Titulacion', Titulacion);
+module.exports = Titulacion;
